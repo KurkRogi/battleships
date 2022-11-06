93c1a13af181b855816a6bbce6cb4e3b78738c93
KurkRogi <igor.k.kruk@gmail.com>
2022-11-06 19:46:47 +0000

    Change code as indicated by pylint to comply with codding stabdard

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-# Battleships terminal text game
+"""Battleships terminal text game"""
+
 import os
 from random import randint
 
@@ -14,17 +15,17 @@ class Board:
     """
     Class for a playing board.
     """
-    
+
     # Unicodes for the board cells
     # Alternative empty cell is 0x2b1e - a dot
     EMPTY_CELL = 0x223c
     HIT_CELL = 0x0058
     SUNKEN_SHIP = 0x002a
     SHIP = 0x25a0
-    
+
     # Unicodes for border. Key: T = top, L = left, R = right,
     #                           M = middle, B = bottom
-    BORDER_TL = 0x2554 
+    BORDER_TL = 0x2554
     BORDER_TM = 0x2550
     BORDER_TR = 0x2557
     BORDER_BL = 0x255a
@@ -37,15 +38,9 @@ class Board:
         self.width = width
         self.height = height
         # Note that the reference to board is y coorinate first
-        self.board = []
+        # Create board as a list or lists filled with empty cells
+        self.board = [[Board.EMPTY_CELL] * self.width for x in range(self.height)]
         self.ships_left = 0
-
-        # Create empty game board
-        for y in range(height):
-            row = []
-            for x in range(width):
-                row.append(Board.EMPTY_CELL)
-            self.board.append(row)
 
     def get_board_line(self, hide_ships=False):
         """
@@ -55,13 +50,13 @@ class Board:
         for i in range(-1, self.height + 1):
             if i == -1:
                 # top border line of the board
-                yield (chr(Board.BORDER_TL) 
+                yield (chr(Board.BORDER_TL)
                        + chr(Board.BORDER_TM) * (self.width * 2 + 1)
                        + chr(Board.BORDER_TR)
                        )
             elif i == self.height:
                 # bottom border line
-                yield (chr(Board.BORDER_BL) 
+                yield (chr(Board.BORDER_BL)
                        + chr(Board.BORDER_BM) * (self.width * 2 + 1)
                        + chr(Board.BORDER_BR)
                        )
@@ -72,7 +67,8 @@ class Board:
                 # Pythonic but still better
                 # than next ternary operator
                 yield (chr(Board. BORDER_LM) + " "
-                       + " ".join(chr(x) if x != Board.SHIP else chr((Board.SHIP, Board.EMPTY_CELL)[hide_ships]) for x in self.board[i])
+                       + " ".join(chr(x) if x != Board.SHIP
+                       else chr((Board.SHIP, Board.EMPTY_CELL)[hide_ships]) for x in self.board[i])
                        + " " + chr(Board.BORDER_RM)
                        )
 
@@ -82,13 +78,13 @@ class Board:
         for debuging purposes only, really.
         """
         return "\n".join(x for x in self.get_board_line(hide_ships))
-     
-    def is_cell_free(self, x, y):
+
+    def is_cell_free(self, x_coord, y_coord):
         """
         Checks if a cell on board is free and returns True if so
         """
 
-        return self.board[y][x] == Board.EMPTY_CELL
+        return self.board[y_coord][x_coord] == Board.EMPTY_CELL
 
     def fill(self, number):
         """
@@ -102,27 +98,27 @@ class Board:
 
         self.ships_left = number
         while number > 0:
-            x = randint(0, self.width - 1)
-            y = randint(0, self.height -1) 
-            if self.is_cell_free(x, y):
-                self.board[y][x] = Board.SHIP
+            x_coord = randint(0, self.width - 1)
+            y_coord = randint(0, self.height -1)
+            if self.is_cell_free(x_coord, y_coord):
+                self.board[y_coord][x_coord] = Board.SHIP
                 number -= 1
         return self.ships_left
 
 
-def get_integer(min=1, max=10, message="Please enter a number [1-10]: "):
+def get_integer(min_value=1, max_value=10, message="Please enter a number [1-10]: "):
     """
     Prompt player to enter an integer within a range
     """
     while True:
         try:
             integer = int(input(message + "\n"))
-            if integer < min or integer > max:
-                raise ValueError(f"{integer} is out of range [{min}-{max}]")
+            if integer < min_value or integer > max_value:
+                raise ValueError(f"{integer} is out of range [{min_value}-{max_value}]")
             break
         except ValueError as error:
             print(f"*** {error} \n")
-    
+
     return integer
 
 
@@ -134,24 +130,25 @@ def get_coordinates(opponent_board):
     """
     min_x, max_x = 1, opponent_board.width
     min_y, max_y = 1, opponent_board.height
-    message = f"Please enter your target's coorinates separated by comma, horizontal first [1-{max_x}],[1-{max_y}]"
-    
+    message = f"Please enter your target's coorinates separated by comma, \
+horizontal first [1-{max_x}],[1-{max_y}]"
+
     while True:
         try:
             data = input(message + "\n").strip().replace(" ", "").split(",")
             if len(data) != 2:
-                raise ValueError(f"Enter 2 numbers separated by comma")
-            x = int(data[0])
-            y = int(data[1])
-            if x < min_x or x > max_x:
+                raise ValueError("Enter 2 numbers separated by comma")
+            x_coord = int(data[0])
+            y_coord = int(data[1])
+            if x_coord < min_x or x_coord > max_x:
                 raise ValueError(f"First coordinate must be between {min_x} and {max_x}")
-            if y < min_y or y > max_y:
+            if y_coord < min_y or y_coord > max_y:
                 raise ValueError(f"Second coordinate must be between {min_y} and {max_y}")
             break
         except ValueError as error:
             print(f"*** {error} \n")
-        
-    return x, y
+
+    return x_coord, y_coord
 
 
 def game_setup():
@@ -163,17 +160,18 @@ def game_setup():
     print("Welcome to the Battleships game\n"
           + "-" * 31 + "\n\n"
           + "*** Game setup")
-        
+
     # Find out what's the size of board player wants
     board_size = get_integer(2, 10, "\nPlease enter the board size [2-10]")
-    
+
     # Find how many ships on the board the player wants
-    number_of_ships = get_integer(1, board_size ** 2, f"\nHow many ships do you want? [1-{board_size ** 2}]")
+    number_of_ships = get_integer(1, board_size ** 2,
+                                  f"\nHow many ships do you want? [1-{board_size ** 2}]")
 
     return (board_size, number_of_ships)
 
 
-def game_instructions(p, o):
+def game_instructions(plr_score, opp_score):
     """
     Display game instructions and current score
     p for player and o for opponent
@@ -187,17 +185,20 @@ def game_instructions(p, o):
 
     print("Your board is on left, opponent's on right")
 
-    print(f"There {('are', 'is')[p == 1]} "
-          + f"still {p} ship{('s', '')[p == 1]} on your "
-          + f"board and {o} on oponent's")
+    print(f"There {('are', 'is')[plr_score == 1]} "
+          + f"still {plr_score} ship{('s', '')[plr_score == 1]} on your "
+          + f"board and {opp_score} on oponent's")
+
+    print("Top left corner coordinates are 1,1")
 
 
 def game_display_boards(player_board, opponent_board):
     """
     Display both player and opponent board next to each other
     """
-    for p, o in zip(player_board.get_board_line(), opponent_board.get_board_line(True)):
-        print(p + " " * 5 + o)
+    for plr_line, opp_line in zip(player_board.get_board_line(),
+                                  opponent_board.get_board_line(True)):
+        print(plr_line + " " * 5 + opp_line)
 
 
 def game_shot(brd, coords):
@@ -206,16 +207,16 @@ def game_shot(brd, coords):
     True if succesfull or False if cell already targeted
     and message to player
     """
-    x, y = coords
-    x -= 1
-    y -= 1
+    x_coord, y_coord = coords
+    x_coord -= 1
+    y_coord -= 1
     result = False
     message = ""
-    cell = brd.board[y][x]
- 
+    cell = brd.board[y_coord][x_coord]
+
     if cell == Board.EMPTY_CELL:
         result = True
-        brd.board[y][x] = Board.HIT_CELL
+        brd.board[y_coord][x_coord] = Board.HIT_CELL
         message = "missed."
     elif cell == Board.HIT_CELL:
         message = "already targeted this coordinates"
@@ -223,14 +224,16 @@ def game_shot(brd, coords):
         message = "already sunk this ship"
     else:
         result = True
-        brd.board[y][x] = Board.SUNKEN_SHIP
+        brd.board[y_coord][x_coord] = Board.SUNKEN_SHIP
         message = "hit a ship!"
         brd.ships_left -= 1
 
     return result, message
 
+
 def main():
-    
+    """ main function runing game loop"""
+
     while True:
         board_size, number_of_ships = game_setup()
 
@@ -238,8 +241,6 @@ def main():
         opponent_board = Board(board_size, board_size)
         player_board.fill(number_of_ships)
         opponent_board.fill(number_of_ships)
-
-        # opponent_board.board[0][0] = Board.HIT_CELL
 
         while True:
             # Ask for coords untill a ship or empty cell is hit
@@ -256,7 +257,7 @@ def main():
                                                 get_coordinates(opponent_board))
 
             # Same procedure for computer opponent but only valid shots
-            # need to display 
+            # need to display
             result = False
             while not result:
                 result, message = game_shot(player_board,
@@ -266,21 +267,21 @@ def main():
             print(f"You {message_player}")
             print(f"Opponent {message}")
             game_display_boards(player_board, opponent_board)
-            
+
             if player_board.ships_left == 0 and opponent_board.ships_left == 0:
                 print("It's a draw!")
-                input("PRESS RETURN\n")
+                input("PRESS RETURN / ENTER\n")
                 break
             if player_board.ships_left == 0:
                 print("You've lost!")
-                input("PRESS RETURN\n")
+                input("PRESS RETURN / ENTER\n")
                 break
             if opponent_board.ships_left == 0:
                 print("You've won!")
-                input("PRESS RETURN\n")
+                input("PRESS RETURN / ENTER\n")
                 break
             if player_board.ships_left and opponent_board.ships_left:
-                input("PRESS RETURN\n")
+                input("PRESS RETURN / ENTER\n")
         break
 
 main()
